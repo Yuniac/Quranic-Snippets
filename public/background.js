3700d5c26f1676500a5522b5616d68394207d22b
Yusuf Sabbag <sist.heb@gmail.com>
2021-10-13 16:53:13 +0300

    no longer overrides stored values

--- public/background.js
+++ public/background.js
@@ -1,11 +1,14 @@
 /* global browser */
-const ayahObject = {
-	ayah: "",
-	ayahTimeStamp: new Date().getTime(),
-	UILang: "en",
-	QLang: "ar.asad",
-	bookmarks: [],
-	isIconFilled: false,
-	freq: 3600000,
-};
-browser.storage.sync.set(ayahObject);
+// only stores the initial values on the very first time the extension runs;
+if (!browser.storage.sync.get()) {
+	const ayahObject = {
+		ayah: "",
+		ayahTimeStamp: new Date().getTime(),
+		UILang: "en",
+		QLang: "ar.asad",
+		bookmarks: [],
+		isIconFilled: false,
+		freq: 3600000,
+	};
+	browser.storage.sync.set(ayahObject);
+}
