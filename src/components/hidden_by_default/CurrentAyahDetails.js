511f954b21266b04ac9617b556ba67eb0c81d88c
Yusuf Sabbag <sist.heb@gmail.com>
2021-10-16 01:43:16 +0300

    added global ayah number, quality of life improvements

--- src/components/hidden_by_default/CurrentAyahDetails.js
+++ src/components/hidden_by_default/CurrentAyahDetails.js
@@ -5,10 +5,11 @@ import ReadEntireSurah from "../mini-components/ReadEntireSurah";
 import Divider from "../shared/Divider";
 //
 function CurrentAyahDetails({ currentAyahDetailsVisibility, UILanguage }) {
-	const [currentSurahNameAR, setCurrentSurahNameAR] = React.useState("الفاتحة");
-	const [currentSurahNameEN, setCurrentSurahNameEN] = React.useState("Al Fatihha");
+	const [currentSurahNameAR, setCurrentSurahNameAR] = React.useState("");
+	const [currentSurahNameEN, setCurrentSurahNameEN] = React.useState("");
 	const [currentAyahNumber, setCurrentAyahNumber] = React.useState(0);
 	const [currentSurahNumber, setcurrentSurahNumber] = React.useState(0);
+	const [currentAyahNumberGlobally, setCurrentAyahNumberGlobally] = React.useState(0);
 
 	// eslint-disable-next-line react-hooks/exhaustive-deps
 	React.useEffect(() => {
@@ -16,9 +17,10 @@ function CurrentAyahDetails({ currentAyahDetailsVisibility, UILanguage }) {
 		getStoredValue(["currentSurahNameEN"], setCurrentSurahNameEN);
 		getStoredValue(["currentAyahNumber"], setCurrentAyahNumber);
 		getStoredValue(["currentSurahNumber"], setcurrentSurahNumber);
+		getStoredValue("currentAyahNumberGlobally", setCurrentAyahNumberGlobally);
 	});
 	return (
-		<div className="current-ayah-details-hidden">
+		<div className="current-ayah-details-hidden" style={{ paddingBottom: currentAyahDetailsVisibility ? "170px" : 0 }}>
 			<div
 				className="wrapper"
 				style={{
@@ -50,25 +52,38 @@ function CurrentAyahDetails({ currentAyahDetailsVisibility, UILanguage }) {
 					</span>
 				</div>
 				<Divider />
-				<div className="row" style={{ flexDirection: UILanguage === "ar" ? "row-reverse" : "row" }}>
+				<div className="row numbers-row" style={{ flexDirection: UILanguage === "ar" ? "row-reverse" : "row" }}>
 					<span>
 						<span style={{ display: UILanguage === "ar" ? "inline-block" : "none", direction: "rtl" }}>
 							<span className="header-span"> رقم الأية في السورة:</span>
-							{currentAyahNumber}
+							<span className="highlight-numbers">{currentAyahNumber}</span>
 						</span>
 						<span style={{ display: UILanguage === "ar" ? "none" : "inline-block", direction: "ltr" }}>
 							<span className="header-span">Ayah number in Surah:</span>
-							{currentAyahNumber}
+							<span className="highlight-numbers">{currentAyahNumber}</span>
 						</span>
 					</span>
 					<span>
 						<span style={{ display: UILanguage === "ar" ? "inline-block" : "none", direction: "rtl" }}>
 							<span className="header-span"> رقم السورة:</span>
-							{currentSurahNumber}
+							<span className="highlight-numbers">{currentSurahNumber}</span>
 						</span>
 						<span style={{ display: UILanguage === "ar" ? "none" : "inline-block", direction: "ltr" }}>
-							<span className="header-span"> Surah number:</span>
-							{currentSurahNumber}25
+							<span className="header-span" style={{ minHeight: UILanguage === "en" ? "72px" : "auto" }}>
+								{" "}
+								Surah number:
+							</span>
+							<span className="highlight-numbers">{currentSurahNumber}</span>
+						</span>
+					</span>
+					<span>
+						<span style={{ display: UILanguage === "ar" ? "inline-block" : "none", direction: "rtl" }}>
+							<span className="header-span"> رقم الأية في القرأن:</span>
+							<span className="highlight-numbers">{currentAyahNumberGlobally}</span>
+						</span>
+						<span style={{ display: UILanguage === "ar" ? "none" : "inline-block", direction: "ltr" }}>
+							<span className="header-span"> Ayah number in the Quran:</span>
+							<span className="highlight-numbers">{currentAyahNumberGlobally}</span>
 						</span>
 					</span>
 				</div>
